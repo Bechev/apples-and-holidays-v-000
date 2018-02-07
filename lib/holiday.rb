20fd32566cad995becd3477aa5a801eda19d1cf6
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-07 12:52:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
     holiday_hash.each do |season, holiday_hash|
-      puts "#{season.capitalize}:\n"
+      puts "#{season.capitalize}:"
         holiday_hash.each do |holiday_key, list_of_items|
         puts "\t#{holiday_key.capitalize}: #{holiday_hash.values.join(", ")}"
 
