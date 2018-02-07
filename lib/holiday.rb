e7867ed9c29b03c798bb903c22c53a34453a89fb
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-07 12:52:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday_hash|
       puts "#{season.capitalize}:"
         holiday_hash.each do |holiday_key, list_of_items|
-        puts "\t#{holiday_key.capitalize}: #{holiday_hash.values.join(", ")}"
+        puts "#{holiday_key.capitalize}: #{holiday_hash.values.join(", ")}"
 
       end
     end
